ec0eb41e274760d8b22b0b233cb5f698dc2e5ead
Kalinin_Vadim <47732165+Vadckacasper@users.noreply.github.com>
2022-08-18 12:25:09 +0700

    Добавьте файлы проекта.

---
+++ EnteringProjectData/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EnteringProjectData.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Suname { get; set; }
+        public string Patronymic { get; set; }
+        public string Email { get; set; }       
+        public IEnumerable<Projects>? Projects { get; set; }
+    }
+}
---
+++ EnteringProjectData/Models/ProjectsEmployee.cs
@@ -0,0 +1,13 @@
+﻿
+namespace EnteringProjectData.Models;
+
+public class ProjectsEmployee
+{
+    public Guid Id { get; set; }
+    public Guid Id_Project { get; set; }
+    public Guid Id_Employee { get; set; }
+    [ForeignKey("Id_Project")]
+    public Projects Project { get; set; } = new Projects();
+    [ForeignKey("Id_Employee")]
+    public Employee Employee { get; set; } = new Employee();
+}
