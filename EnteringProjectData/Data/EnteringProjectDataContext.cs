5e3901141a0feff319da7e6a460397293c2c4cca
Kalinin_Vadim <47732165+Vadckacasper@users.noreply.github.com>
2022-08-19 16:11:29 +0700

    Data context for working with the database

---
+++ EnteringProjectData/Data/EnteringProjectDataContext.cs
@@ -0,0 +1,19 @@
+﻿namespace EnteringProjectData.Data;
+
+public class EnteringProjectDataContext:DbContext
+{
+    public DbSet<Project> Projects { get; set; } = null!;
+    public DbSet<Employee> Employees { get; set; } = null!;
+    public DbSet<ProjectsEmployee> ProjectsEmployees { get; set; } = null!;
+
+    public EnteringProjectDataContext(DbContextOptions<EnteringProjectDataContext> options):base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Project>().Property(u => u.StartDates).HasColumnType("date");
+        modelBuilder.Entity<Project>().Property(u => u.EndDates).HasColumnType("date");
+    }
+}
