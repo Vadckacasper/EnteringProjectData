3fb3a730fa50dc0c6aebacaee812d7ee9b641a14
Kalinin_Vadim <47732165+Vadckacasper@users.noreply.github.com>
2022-08-19 16:14:19 +0700

    File with libraries

--- EnteringProjectData/Import.cs
+++ EnteringProjectData/Import.cs
@@ -1,2 +1,6 @@
 ﻿global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
+global using Microsoft.EntityFrameworkCore;
+global using EnteringProjectData.Data;
+
+global using EnteringProjectData.Models;
