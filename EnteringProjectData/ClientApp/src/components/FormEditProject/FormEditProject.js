82a42b45d7cdd30ea6d811b1ce07dab4f64e7bb1
Kalinin_Vadim <47732165+Vadckacasper@users.noreply.github.com>
2022-08-20 20:47:44 +0700

    Updating the project

---
+++ EnteringProjectData/ClientApp/src/components/FormEditProject/FormEditProject.js
@@ -0,0 +1,130 @@
+import React from "react";
+import {useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { FormCreateEmployee } from "../FormCreateEmployee/FormCreateEmployee";
+export function FormEditProject() {
+    const [Data, setData] = useState(0);
+    const params = useParams();
+    const Id = params.id;
+
+    useEffect(() => {
+        Load();
+      },[Id]);
+
+    const Load = async() => {
+        const response = await fetch(`api/projects/${Id}`);
+        if (response.ok) {
+        const data = await response.json();
+        setData(data);
+        }
+      }
+
+  const Put = async (event) => {
+    await event.preventDefault();
+
+    const project = {
+      Id: Id,
+      Name: Data.name,
+      CustomerCompany: Data.customerCompany,
+      ImplementingCompany: Data.implementingCompany,
+      StartDates: Data.startDates,
+      EndDates: Data.endDates,
+      Priority: Data.priority,
+      Id_Manager: 1,
+    };
+
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(project),
+    };
+    await fetch(`/api/projects/${Id}`, requestOptions);
+  };
+
+  const handleInputChange = (e) => {
+    setData({...Data, [e.target.name]: e.target.value});
+}
+
+  return (
+    <div>
+    <form onSubmit={Put} id="create-project-form">
+      <div className="mb-3">
+        <label className="form-label">Название проекта</label>
+        <input
+          type="text"
+          className="form-control"
+          name="name"
+          maxLength={50}
+          value={Data.name}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Компания заказчик</label>
+        <input
+          type="text"
+          className="form-control"
+          name="customerCompany"
+          maxLength={50}
+          value={Data.customerCompany}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Компания исполнитель</label>
+        <input
+          type="text"
+          className="form-control"
+          name="implementingCompany"
+          maxLength={50}
+          value={Data.implementingCompany}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div className="row">
+        <div className="col">
+          <label className="form-label">Дата начала</label>
+          <input
+            type="date"
+            className="form-control"
+            name="startDates"
+            
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="col">
+          <label className="form-label">Дата окончания</label>
+          <input
+            type="date"
+            className="form-control"
+            name="endDates"
+            
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="col">
+          <label className="form-label">Приоритет</label>
+          <input
+            type="number"
+            min="1"
+            className="form-control"
+            name="priority"
+            value={Data.priority}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+      </div>
+      <button type="submit" className="btn btn-outline-primary mt-2">
+        Отправить
+      </button>
+    </form>
+    <FormCreateEmployee></FormCreateEmployee>
+    </div>
+  );
+}
