d3e7f4da81cb334e6aab7e4e3116768e2e0b9fd2
Kalinin_Vadim <47732165+Vadckacasper@users.noreply.github.com>
2022-08-22 20:28:07 +0700

    added a controller for working with the relationship table

---
+++ EnteringProjectData/Controllers/ProjectsEmployeesController.cs
@@ -0,0 +1,57 @@
+﻿
+using EnteringProjectData.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EnteringProjectData.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProjectsEmployeesController : ControllerBase
+{
+    private readonly EnteringProjectDataContext _context;
+
+    public ProjectsEmployeesController(EnteringProjectDataContext context)
+    {
+        _context = context;
+    }
+    // POST: api/ProjectsEmployees
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<ProjectsEmployee>> PostProjectsEmployee([FromBody] ProjectsEmployee projectsEmployee)
+    {
+        if (_context.ProjectsEmployees == null)
+        {
+            return Problem("Entity set 'EnteringProjectDataContext.ProjectsEmployees'  is null.");
+        }
+        _context.ProjectsEmployees.Add(projectsEmployee);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetProject", new { id = projectsEmployee.Id }, projectsEmployee);
+    }
+
+    [HttpDelete("{idProject}:{idEnployee}")]
+    public async Task<IActionResult> DeleteEmployee(int idProject, int idEnployee)
+    {
+        if (_context.Employees == null)
+        {
+            return NotFound();
+        }
+        var projectEmployee = await _context.ProjectsEmployees.Where(p => p.Id_Project == idProject && p.Id_Employee==idEnployee).FirstOrDefaultAsync();
+        if (projectEmployee == null)
+        {
+            return NotFound();
+        }
+
+        _context.ProjectsEmployees.Remove(projectEmployee);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+
+    private bool ProjectsEmployeeExists(int id)
+    {
+        return (_context.ProjectsEmployees?.Any(e => e.Id == id)).GetValueOrDefault();
+    }
+}
