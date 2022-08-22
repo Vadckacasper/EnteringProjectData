130f5e381690310f1819beaf4743adb28d762e0c
Kalinin_Vadim <47732165+Vadckacasper@users.noreply.github.com>
2022-08-22 21:02:31 +0700

    Navigation changed

--- EnteringProjectData/ClientApp/src/components/NavMenu/NavMenu.js
+++ EnteringProjectData/ClientApp/src/components/NavMenu/NavMenu.js
@@ -30,13 +30,10 @@ export class NavMenu extends Component {
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                <NavLink tag={Link} className="text-dark" to="/">Список проектов</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                <NavLink tag={Link} className="text-dark" to="/employees">Список сотрудников</NavLink>
               </NavItem>
             </ul>
           </Collapse>
