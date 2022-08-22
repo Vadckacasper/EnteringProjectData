35a89f04e2fe4b807c3cadb070e77be392b781e0
Kalinin_Vadim <47732165+Vadckacasper@users.noreply.github.com>
2022-08-22 21:03:00 +0700

    added new components to the form

--- EnteringProjectData/ClientApp/src/components/Home/Home.js
+++ EnteringProjectData/ClientApp/src/components/Home/Home.js
@@ -1,30 +1,32 @@
 import React from "react";
-import {useState, useEffect } from "react";
-import {TableProjects} from "../TableProjects/TableProjects";
+import { useState, useEffect } from "react";
+import { TableProjects } from "../TableProjects/TableProjects";
+import { FormFilterDate } from "../FormFilterDate/FormFilterDate";
 import { Link } from "react-router-dom";
-export function Home(){
-
+export function Home() {
   const [Data, setData] = useState([]);
 
   useEffect(() => {
     Load();
-  },[Data]);
+  }, []);
 
-const Load = async() => {
-    const response = await fetch('api/projects');
-    if(response.ok)
-    {
-    const data = await response.json();
-    setData(data);
+  const Load = async () => {
+    const response = await fetch("api/projects");
+    if (response.ok) {
+      const data = await response.json();
+      setData(data);
     }
+  };
+
+  const Update = (data) => {
+    setData(data);
   }
 
-
-    return(
-      <div>
-       <TableProjects data={Data} />
-        <Link to={"/project/create"}>Создать проект</Link>
-      </div>
-    );
-  
+  return (
+    <div>
+      <FormFilterDate update={Update} />
+      <TableProjects update={Load} data={Data} />
+      <Link to={"/project/create"}>Создать проект</Link>
+    </div>
+  );
 }
